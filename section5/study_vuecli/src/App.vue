94c0e230e4ecf99d744dbb4ca513c74d8989d905
kai815 <h.kaizuka815@gmail.com>
2019-11-16 15:20:17 +0900

    study section8component slot keep-alive...etc

--- section5/study_vuecli/src/App.vue
+++ section5/study_vuecli/src/App.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <h2>{{ number }}</h2>
+    <LikeHeader>
+    </LikeHeader>
     <LikeNumber v-bind:total-number="number" v-on:my-click="number = $event"></LikeNumber>
-    <LikeNumber v-bind:total-number="number" test-props="テスト"></LikeNumber>
+    <button v-on:click="currentComponet = 'Home'">Home</button>
+    <button v-on:click="currentComponet = 'About'">About</button>
+    <keep-alive>
+      <component v-bind:is="currentComponet"></component>
+    </keep-alive>
   </div>
 </template>
 <script>
 import LikeHeader from "./components/LikeHeader.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 export default {
   data(){
       return {
           number:10,
+          currentComponet: "Home"
       }
   },
   components: {
-      LikeHeader
+      LikeHeader,
+      About,
+      Home,
   }
 }
 </script>
@@ -23,5 +32,8 @@ export default {
 <style scoped>
 div {
   border: 1px solid blue;
+}
+h1 {
+  color: red;
 }
 </style>
