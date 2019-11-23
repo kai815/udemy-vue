c45b3a3bcefa7a7355804cefb51ffca5100c911c
kai815 <h.kaizuka815@gmail.com>
2019-11-23 13:39:20 +0900

    study section13 vue-router

---
+++ section13/study_vue_router/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from "./router"
+
+Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+  if (to.path === "users/1") {
+    next({path: '/'});
+  }
+  next();
+})
+
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
---
+++ section13/study_vue_router/src/views/Users.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h3>Users</h3>
+      <router-link to="/users/1">ユーザー1</router-link>
+      <router-link to="/users/2">ユーザー2</router-link>
+      <hr>
+      <!-- <h1>User No. {{ $route.params.id }}</h1> -->
+      <h1>User No. {{ id }}</h1>
+      <router-link :to="'/users/'+ (Number(id) + 1) + '/profile?lang=ja&hash=#next-user'">次のユーザーのプロフィール</router-link>
+      <router-view></router-view>
+      <div style="height: 700px;"></div>
+      <router-link id="next-user" :to="{ name: 'users-id-profile',params: {id: Number(id) + 1}, query: { lang:'ja', page: 2}, hash:'#next-user'}">次のユーザーのプロフィール</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["id"],
+}
+</script>
